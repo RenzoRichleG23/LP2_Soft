3121af28ef3c562c2da7051330a150ad29192d32
OSCAR DANIEL <OSCAR DANIEL@DESKTOP-4NV5J8G>
2021-09-15 10:00:38 -0500

    Definicion basica de las clases: Lab2

---
+++ GestUsuarios/src/pe/edu/pucp/LP2Soft/GestUsuarios/model/Ubicacion.java
@@ -0,0 +1,8 @@
+/**
+ * @author INFunables Group
+ */
+package pe.edu.pucp.LP2Soft.GestUsuarios.model;
+
+enum Ubicacion {
+    Lima_Metropolitana, Pueblo_Libre, San_Miguel, Callao, San_Isidro, Miraflores
+}
