5b37f2e5cb09f8e5627782500403751c18e1e94b
OSCAR DANIEL <OSCAR DANIEL@DESKTOP-4NV5J8G>
2021-09-26 15:43:30 -0500

    Todos los archivos declarados - Principal

---
+++ LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
@@ -0,0 +1,9 @@
+/**
+ * @author INFunables Group
+ */
+package pe.edu.pucp.LP2Soft.principal.main;
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Programa Finalizado con exito!");
+    }
+}
