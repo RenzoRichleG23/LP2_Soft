2578449ce054ded9a55bf069985ca7e08df003fe
SandroHM26 <70769239+SandroHM26@users.noreply.github.com>
2021-09-26 21:42:56 -0500

    Toy haciendo la clase principal :D

--- LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
+++ LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
@@ -2,8 +2,17 @@
  * @author INFunables Group
  */
 package pe.edu.pucp.LP2Soft.principal.main;
+
+import pe.edu.pucp.LP2Soft.controller.dao.GestCursos.CursoDAO;
+import pe.edu.pucp.LP2Soft.controller.mysql.GestCursos.CursoMySQL;
+import pe.edu.pucp.LP2Soft.model.GestCursos.Curso;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Programa Finalizado con exito!");
+        Curso c1 = new Curso("INF239","Sistemas Operativos",4.0f,
+        "Informática",7,"Curso Complejo",0.0f,0);
+        CursoDAO daoCurso = new CursoMySQL();
+        daoCurso.insertar(c1);
+        System.out.println("Salida con exito");
     }
 }
