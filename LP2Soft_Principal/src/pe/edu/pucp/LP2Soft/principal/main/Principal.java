a61d5f44f000f4f860b1dc877af05950970ef074
SandroHM26 <70769239+SandroHM26@users.noreply.github.com>
2021-09-28 10:50:53 -0500

    Cambiando el principal

--- LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
+++ LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
@@ -13,9 +13,11 @@ import pe.edu.pucp.LP2Soft.model.GestUsuarios.Usuario;
 import java.util.Date;
 import pe.edu.pucp.LP2Soft.controller.dao.GestUsuarios.UsuarioDAO;
 import pe.edu.pucp.LP2Soft.controller.mysql.GestUsuarios.UsuarioMySQL;
-
 public class Principal {
     public static void main(String[] args) {
+        int i = 4;
+        if( i == 4){
+            System.out.println("Soy el numero "+i);
         Curso c1 = new Curso("INF239","Sistemas Operativos",4.0f,
         "Informática",7,"Curso Complejo",0.0f,0);
         CursoDAO daoCurso = new CursoMySQL();
@@ -49,5 +51,9 @@ public class Principal {
             System.out.println(ex.getMessage());
         }
         System.out.println("Programa Finalizado con exito!");
+        
+        
+        
     }
+    
 }
