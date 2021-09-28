cb273b18d394bea9373dfd85d398ba3c704f06ad
OSCAR DANIEL <OSCAR DANIEL@DESKTOP-4NV5J8G>
2021-09-27 19:21:38 -0500

    Usuario check! - Principal Merge :( 5.3

--- LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
+++ LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal.java
@@ -6,6 +6,13 @@ package pe.edu.pucp.LP2Soft.principal.main;
 import pe.edu.pucp.LP2Soft.controller.dao.GestCursos.CursoDAO;
 import pe.edu.pucp.LP2Soft.controller.mysql.GestCursos.CursoMySQL;
 import pe.edu.pucp.LP2Soft.model.GestCursos.Curso;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import pe.edu.pucp.LP2Soft.model.GestUsuarios.Usuario;
+import java.util.Date;
+import pe.edu.pucp.LP2Soft.controller.dao.GestUsuarios.UsuarioDAO;
+import pe.edu.pucp.LP2Soft.controller.mysql.GestUsuarios.UsuarioMySQL;
 
 public class Principal {
     public static void main(String[] args) {
@@ -14,5 +21,33 @@ public class Principal {
         CursoDAO daoCurso = new CursoMySQL();
         daoCurso.insertar(c1);
         System.out.println("Salida con exito");
+        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            // Insertar
+//            Date fechaN = formato.parse("01-01-2000");
+//            Usuario u = new Usuario(20216008,"Julanito Gonzales","julanito.g@pucp.edu.pe","Ing. Informática", "julanitoxd", fechaN, "Hola, soy un usuario de pruebas. Soy tu amigo Julanito :D");
+            UsuarioDAO usuarioDao = new UsuarioMySQL();
+//            usuarioDao.insertar(u);
+            // Modificar
+//            Date nuevaFecha = formato.parse("27-02-1999");
+//            u.setFechaNacimiento(nuevaFecha);
+//            usuarioDao.modificar(u);
+            // Eliminar
+//            usuarioDao.eliminar(20216008);
+            // Listar todos
+            ArrayList<Usuario> usuarios = usuarioDao.listarTodos();
+            for(Usuario us : usuarios) {
+                System.out.println(us.getCodigoPUCP()+ "  " + us.getNombre()+ " - " + formato.format(us.getFechaNacimiento()));
+            }
+            // Mostrar
+            Usuario us = usuarioDao.mostrar(20186008, "oscar20186008");
+            if(us!=null)
+                System.out.println(us.getCodigoPUCP()+ "  " + us.getNombre()+ " - " + formato.format(us.getFechaNacimiento()));
+            else System.out.println("Usuario no encontrado");
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Programa Finalizado con exito!");
     }
 }
