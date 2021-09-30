df4b192b8048f13da50b6d210a766570e8c0f9ed
SandroHM26 <70769239+SandroHM26@users.noreply.github.com>
2021-09-29 23:29:39 -0500

    Sandro a superado el Merge

--- LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal_Curso.java
+++ LP2Soft_Principal/src/pe/edu/pucp/LP2Soft/principal/main/Principal_Curso.java
@@ -5,9 +5,11 @@
  */
 package pe.edu.pucp.LP2Soft.principal.main;
 
+import java.util.ArrayList;
 import pe.edu.pucp.LP2Soft.controller.dao.GestCursos.CursoDAO;
 import pe.edu.pucp.LP2Soft.controller.mysql.GestCursos.CursoMySQL;
 import pe.edu.pucp.LP2Soft.model.GestCursos.Curso;
+import pe.edu.pucp.LP2Soft.model.GestCursos.Profesor;
 
 /**
  *
@@ -15,8 +17,21 @@ import pe.edu.pucp.LP2Soft.model.GestCursos.Curso;
  */
 public class Principal_Curso {
    public static void main(String[] args) {
-        Curso c = new Curso("INF282","LENGUAJES DE PROGRAMACION 2",5.00f,"INGENIERIA INFORMATICA",7,"CURSO DE SÉPTIMO CICLO",0,0);
-        CursoDAO cDao = new CursoMySQL();
-        cDao.insertar(c);
+        //Curso c = new Curso("INF282","LENGUAJES DE PROGRAMACION 2",5.00f,"Ing. Informática",7,"CURSO DE SÉPTIMO CICLO",0,0);
+        //Curso c2 = new Curso("INF619","LENGUAJES DE PROGRAMACION 2",5.00f,"Ing. Informática",7,"CURSO DE SÉPTIMO CICLO",0,0);
+        //CursoDAO cDao = new CursoMySQL();
+        //cDao.insertar(c);
+        //cDao.insertar(c2);
+        //cDao.eliminar("INF619");
+        //cDao.eliminar("INF666");
+        //ArrayList<Curso> cursos;
+        ///CursoDAO daoCurso = new CursoMySQL();
+        //cursos=daoCurso.listarTodos();
+        //Curso curso;
+        ///curso = cursos.get(0);
+        //System.out.println(curso.getCodigo());
+        //curso.setDescripcion("CURSO DE SEPTIMO CICLO DICTADO POR FREDDY PAZ");
+       /// daoCurso.modificar(curso);
+       Profesor p1 = new Profesor();
    }
 }
