fd4a904d095ae7d2f448c832474fdee5e094b3e1
JOSE <JOSE@DESKTOP-SJ43T6E>
2021-09-29 19:08:07 -0500

    José

--- LP2Soft_Controller/src/pe/edu/pucp/LP2Soft/controller/dao/GestPublicaciones/PostDAO.java
+++ LP2Soft_Controller/src/pe/edu/pucp/LP2Soft/controller/dao/GestPublicaciones/PostDAO.java
@@ -3,6 +3,12 @@
  */
 package pe.edu.pucp.LP2Soft.controller.dao.GestPublicaciones;
 
-public interface PostDAO {
+import java.util.ArrayList;
+import pe.edu.pucp.LP2Soft.model.GestPublicaciones.Post;
 
+public interface PostDAO {
+    int insertar(Post post);
+    int meodificar(Post post);
+    int eliminar(Post post);
+    ArrayList<Post> listarTodos();
 }
--- LP2Soft_Controller/src/pe/edu/pucp/LP2Soft/controller/dao/GestPublicaciones/ReseniaDAO.java
+++ LP2Soft_Controller/src/pe/edu/pucp/LP2Soft/controller/dao/GestPublicaciones/ReseniaDAO.java
@@ -1,8 +1,16 @@
 /**
  * @author INFunables Group
  */
+
 package pe.edu.pucp.LP2Soft.controller.dao.GestPublicaciones;
 
-public interface ReseniaDAO {
+import java.util.ArrayList;
+import pe.edu.pucp.LP2Soft.model.GestPublicaciones.Resenia;
 
+
+public interface ReseniaDAO {
+    int insertar(Resenia resenia);
+    int meodificar(Resenia resenia);
+    int eliminar(Resenia resenia);
+    ArrayList<Resenia> listarTodos();
 }
