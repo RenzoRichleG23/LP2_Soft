41f9fbd8fc98941246f17f3d4fe0570197089108
OSCAR DANIEL <OSCAR DANIEL@DESKTOP-4NV5J8G>
2021-10-29 22:03:47 -0500

    Agregué las clases en WS

---
+++ LP2Soft_WebServices/src/java/pe/edu/pucp/LP2Soft/services/CursosWS.java
@@ -0,0 +1,24 @@
+/**
+ * @author INFunables Group
+ */
+package pe.edu.pucp.LP2Soft.services;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author OSCAR DANIEL
+ */
+@WebService(serviceName = "CursosWS")
+public class CursosWS {
+
+    /**
+     * Aquí van a crearse todos los métodos de acceso para el paquete de Cursos
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
---
+++ LP2Soft_WebServices/src/java/pe/edu/pucp/LP2Soft/services/PublicacionesWS.java
@@ -0,0 +1,20 @@
+/**
+ * @author INFunables Group
+ */
+package pe.edu.pucp.LP2Soft.services;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+@WebService(serviceName = "PublicacionesWS")
+public class PublicacionesWS {
+
+    /**
+     * Aquí van a crearse todos los métodos de acceso para el paquete de Publicaciones
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
